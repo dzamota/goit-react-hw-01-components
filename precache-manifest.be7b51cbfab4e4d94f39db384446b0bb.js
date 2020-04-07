427689a8df6bde016be83c07017f4d478c4aee27
dzamota <d.zamota@gmail.com>
2020-04-07 20:01:46 +0300

    Updates

---
+++ precache-manifest.be7b51cbfab4e4d94f39db384446b0bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8562b86374094b4e1cde068c9e303361",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "fbfd111bd6f2c6b87667",
+    "url": "/goit-react-hw-01-components/static/css/main.434b1aa6.chunk.css"
+  },
+  {
+    "revision": "1f52a8114761f12c263c",
+    "url": "/goit-react-hw-01-components/static/js/2.f6b3ea8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.f6b3ea8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbfd111bd6f2c6b87667",
+    "url": "/goit-react-hw-01-components/static/js/main.15078b6f.chunk.js"
+  },
+  {
+    "revision": "f41507ba7c9aee7f0098",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.0100adac.js"
+  }
+]);
